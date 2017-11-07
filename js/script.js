a081bba3278c1cc3e20c631a81529ea2eebc818a
RivasAlejandro <joalrivashe@ittepic.edu.mx>
2017-11-07 07:15:58 -0700

    mensaje

--- js/script.js
+++ js/script.js
@@ -2,9 +2,16 @@
  * Created by ale on 13/10/17.
  */
 $(document).ready(function (){
+    $('.card').on("click","a,img", function (e) {
+        var imageSrc = $(this).parents(".card").find("img").attr("src");
+        $(".js-download").attr("href", imageSrc);
+        $(".js-modal-image").attr("src", imageSrc);
+        $("#modalPicture").modal();
 
 
 
+
+    });
 
 
 
